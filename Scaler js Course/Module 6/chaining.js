e390cb715ebf2f80a9f110109c1f1f0cf5bb01f4
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-02-26 23:09:48 +0530

    Add files via upload

--- Scaler js Course/Module 6/chaining.js
+++ Scaler js Course/Module 6/chaining.js
@@ -9,4 +9,31 @@ let arr = [
     {name: "H", age: 47, gender: "F"},
 ]
 
- // Age od All the Men(Males)
+// Age od All the Men(Males)
+// Chaning  
+ let males = arr.filter(function(obj){
+    return obj.gender == "M"
+ }).map(function(male){
+    return male.age
+ })
+
+ console.log(males)
+ 
+ // Normal
+ 
+// let agesOfmales = males.map(function(male){
+ //   return male.age
+ //})
+
+ //console.log(agesOfmales)
+
+ // From the Transactions Array filter out positive Elements and Calculate the total amount
+ // Use filter and Reduce to Achieve this 
+
+ const Transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500]
+
+ let TotalAmount = arr.filter(function(obj){
+    return obj.Transactions == (n % 2 == 0)
+ })
+
+ console.log(TotalAmount)
